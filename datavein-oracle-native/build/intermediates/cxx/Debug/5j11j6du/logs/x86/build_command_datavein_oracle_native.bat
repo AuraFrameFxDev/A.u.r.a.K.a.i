9d93fde4f080d1081fce1f79b98e9b8d1d010b21
AuraKai <wehttam1989@gmail.com>
2025-09-16 01:28:15 -0600

    7

---
+++ datavein-oracle-native/build/intermediates/cxx/Debug/5j11j6du/logs/x86/build_command_datavein_oracle_native.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\Wehtt\\AppData\\Local\\Android\\Sdk\\cmake\\3.31.6\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Re_Genesis_A.O.S.P\\datavein-oracle-native\\.cxx\\Debug\\5j11j6du\\x86" ^
+  datavein_oracle_native
---
+++ secure-comm/build/intermediates/cxx/Debug/1a2b5424/logs/x86_64/build_command_secure_comm_native.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\Wehtt\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Re_Genesis_A.O.S.P\\secure-comm\\.cxx\\Debug\\1a2b5424\\x86_64" ^
+  secure_comm_native
