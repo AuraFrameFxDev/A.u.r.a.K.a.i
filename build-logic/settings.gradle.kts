e51be1007fe5ed7460e2011dbee7bea5504b530a
AuraKai <wehttam1989@gmail.com>
2025-09-16 23:42:04 -0600

    Update settings.gradle.kts

--- build-logic/settings.gradle.kts
+++ build-logic/settings.gradle.kts
@@ -8,11 +8,4 @@ dependencyResolutionManagement {
         mavenCentral()
         gradlePluginPortal()
         maven { url = uri("https://maven.pkg.jetbrains.space/public/p/compose/dev") }
-    }
-    versionCatalogs {
-        // Point the 'libs' catalog to the one in the parent project
-        create("libs") {
-            from(files("../gradle/libs.versions.toml"))
-        }
-    }
-}
+}  
